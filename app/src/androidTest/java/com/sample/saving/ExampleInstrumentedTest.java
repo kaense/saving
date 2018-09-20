d2054fcd48fb35088fc135c4f685e7c038d36308
kaense <pmwt12@gmail.com>
2018-09-20 22:46:22 +0900

    2

--- app/src/androidTest/java/com/sample/saving/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sample/saving/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sample.inkyolife", appContext.getPackageName());
+        assertEquals("com.sample.saving", appContext.getPackageName());
     }
 }
